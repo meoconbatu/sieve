7388a77ae0587196b92d19c9264169b0254fd79e
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2017-04-14 23:35:01 +0700

    optimize

--- sieve.go
+++ sieve.go
@@ -4,18 +4,23 @@ import "math"
 
 const testVersion = 1
 
-func Sieve(input int) (p []int) {
+func Sieve(input int) []int {
 	sqrtInput := int(math.Sqrt(float64(input)))
 	a := make([]bool, input)
-	for i := 2; i < input; i++ {
+	for i := 2; i <= sqrtInput; i++ {
 		if a[i] == false {
-			p = append(p, i)
-			if i <= sqrtInput {
-				for j := i; j < input; j = j + i {
-					a[j] = true
-				}
+			for j := i * i; j < input; j = j + i {
+				a[j] = true
 			}
 		}
 	}
-	return
+	np := 0
+	p := make([]int, input)
+	for i := 2; i < input; i++ {
+		if a[i] == false {
+			p[np] = i
+			np++
+		}
+	}
+	return p[0:np]
 }
